3636c6612a9f74c5b7effc2b36ca5c8aabee8d15
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2023-04-16 20:33:44 -0700

    Add excerpts and remove redundant CSS

--- questions.js
+++ questions.js
@@ -1,103 +1,110 @@
 let pageTest;
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch("practice-test-" + testNumber + ".json").then(response => response.json()).then(setupTest)
+  fetch("practice-test-" + testNumber + ".json").then(response => response.json()).then(setupTest)
 });
 
 function setupTest(test) {
-    pageTest = test
-    let body = document.getElementsByTagName("body")[0]
-    let i = 0
+  pageTest = test
+  let body = document.getElementsByTagName("body")[0]
+  let i = 0
 
-    for (const question of test.questions) {
-        let element = document.createElement("div")
-        element.classList.add("question")
+  for (const question of test.questions) {
+    let element = document.createElement("div")
+    element.classList.add("question")
 
-        let questionNumber = document.createElement("b")
-        questionNumber.textContent = "Question " + (i + 1)
+    let questionNumber = document.createElement("b")
+    questionNumber.textContent = "Question " + (i + 1)
 
-        let prompt = document.createElement("p")
-        prompt.append(question.prompt)
+    let prompt = document.createElement("p")
+    prompt.append(question.prompt)
 
-        let passage;
-        if (question.poetry) {
-            // table with line numbers
-            passage = document.createElement("div")
-            passage.classList.add("poetry")
-            let poem = document.createElement("div")
-            poem.classList.add("poem")
-            let lineCount = 0
-            for (const line of question.passage.split("\n")) {
-                lineCount++
-                let br = document.createElement("br")
-                poem.append(line, br)
-            }
-            let lineNumbers = document.createElement("div");
-            lineNumbers.classList.add("line-numbers")
-            for (let i = 0; i <= lineCount; i++) {
-                if ((i + 1) % 5 === 0) {
-                    lineNumbers.append("" + (i + 1))
-                }
-                lineNumbers.append(document.createElement("br"))
-            }
-            passage.append(lineNumbers, poem)
-        } else {
-            // paragraph
-            passage = document.createElement("p")
-            passage.classList.add("passage")
-            passage.textContent = question.passage
-        }
-
-        let answers = document.createElement("form")
-        answers.onsubmit = checkAnswers
-        let j = 0
-        for (const choice of question.choices) {
-            let answer = document.createElement("input")
-            let label = document.createElement("label")
-            answer.type = "checkbox"
-            answer.id = "q" + i + ";" + j
-            label.htmlFor = answer.id
-            label.textContent = choice.title
-
-            answers.append(answer, label, document.createElement("br"))
-            j++
-        }
-        let check = document.createElement("input")
-        check.type = "submit"
-        check.value = "Check Answer"
-        answers.appendChild(check)
-
-        let feedback = document.createElement("p")
-        feedback.id = "q" + i
-
-        element.append(questionNumber, prompt, passage, answers, feedback)
-        body.appendChild(element)
-        i++
+    let passage;
+    if (question.poetry) {
+      passage = document.createElement("div")
+      makePoemLineNumbers(passage)
+    } else {
+      // paragraph
+      passage = document.createElement("p")
+      passage.classList.add("passage")
+      passage.textContent = question.passage
     }
+
+    let answers = document.createElement("form")
+    answers.onsubmit = checkAnswers
+    let j = 0
+    for (const choice of question.choices) {
+      let answer = document.createElement("input")
+      let label = document.createElement("label")
+      answer.type = "checkbox"
+      answer.id = "q" + i + ";" + j
+      label.htmlFor = answer.id
+      label.textContent = choice.title
+
+      answers.append(answer, label, document.createElement("br"))
+      j++
+    }
+    let check = document.createElement("input")
+    check.type = "submit"
+    check.value = "Check Answer"
+    answers.appendChild(check)
+
+    let feedback = document.createElement("p")
+    feedback.id = "q" + i
+
+    element.append(questionNumber, prompt, passage, answers, feedback)
+    body.appendChild(element)
+    if (i + 1 < test.questions.length) {
+      body.append(document.createElement("hr"))
+    }
+    i++
+  }
+}
+
+function makePoemLineNumbers(passage) {
+  // table with line numbers
+  passage.classList.add("poetry")
+  let poem = document.createElement("div")
+  let lineCount = 0
+  for (const line of question.passage.split("\n")) {
+    lineCount++
+    let br = document.createElement("br")
+    poem.append(line, br)
+  }
+  let lineNumbers = document.createElement("div");
+  lineNumbers.classList.add("line-numbers")
+  for (let i = 0; i <= lineCount; i++) {
+    if ((i + 1) % 5 === 0) {
+      lineNumbers.append("" + (i + 1))
+    }
+    lineNumbers.append(document.createElement("br"))
+  }
+
+  passage.append(lineNumbers, poem)
 }
 
 function checkAnswers(form) {
-    let choices = form.target.length - 1
+  let choices = form.target.length - 1
 
-    for (let i = 0; i < choices; i++) {
-        let choice = form.target[i];
-        let t = choice.id.substring(1).split(";");
-        let question = pageTest.questions[t[0]];
-        let answer = question.choices[t[1]].correct;
-        let feedback = document.getElementById("q" + t[0]);
+  for (let i = 0; i < choices; i++) {
+    let choice = form.target[i];
+    let t = choice.id.substring(1).split(";");
+    let question = pageTest.questions[t[0]];
+    let answer = question.choices[t[1]].correct;
+    let feedback = document.getElementById("q" + t[0]);
 
-        if (choice.checked !== answer) {
-            feedback.classList.add("wrong-answer");
-            feedback.textContent = "Incorrect answer"
-            break;
-        }
-
-        if (i + 1 === choices) {
-            feedback.classList.remove("wrong-answer");
-            feedback.classList.add("correct-answer");
-            feedback.textContent = "Correct!"
-        }
+    if (choice.checked !== answer) {
+      feedback.classList.add("wrong-answer");
+      feedback.textContent = "Incorrect answer"
+      break;
     }
 
-    return false
+    if (i + 1 === choices) {
+      feedback.classList.remove("wrong-answer");
+      feedback.classList.add("correct-answer");
+      feedback.textContent = "Correct! 😎"
+    }
+  }
+
+  return false
 }
